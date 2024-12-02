5902c94207c20f9a76048bb65a68c9b3283f176e
Sakib Hasan <shasan@lawdepot.net>
2024-12-02 11:47:34 -0700

    refactor

--- Day2/part1.py
+++ Day2/part1.py
@@ -1,21 +1,21 @@
-def part1(reports):
-    def is_safe(report):
-        n = len(report)
-        if n<2:
-            return True # single level is usually safe
+def is_safe(report):
+    n = len(report)
+    if n<2:
+        return True # single level is usually safe
         
-        direction = None
-        for i in range(1, n):
-            diff = report[i] - report[i-1]
+    direction = None
+    for i in range(1, n):
+        diff = report[i] - report[i-1]
 
-            if not(1<=abs(diff)<=3):
-                return False
-            if direction is None:
-                direction = diff
-            elif (direction > 0 and diff < 0) or (direction < 0 and diff > 0):
-                return False
-        return True
-    
+        if not(1<=abs(diff)<=3):
+            return False
+        if direction is None:
+            direction = diff
+        elif (direction > 0 and diff < 0) or (direction < 0 and diff > 0):
+            return False
+    return True
+
+def part1(reports):    
     count = 0
     for report in reports:
         levels = list(map(int, report.split()))
--- Day2/part2.py
+++ Day2/part2.py
@@ -1,21 +1,5 @@
-def part2(reports):
-    def is_safe(report):
-        n = len(report)
-        if n<2:
-            return True # single level is usually safe
-        
-        direction = None
-        for i in range(1, n):
-            diff = report[i] - report[i-1]
-
-            if not(1<=abs(diff)<=3):
-                return False
-            if direction is None:
-                direction = diff
-            elif (direction > 0 and diff < 0) or (direction < 0 and diff > 0):
-                return False
-        return True
-    
+from part1 import is_safe
+def part2(reports):    
     def is_safe_dampener(report):
         if is_safe(report):
             return True

19d3b2dcbff3bb3f1e3eb79594758e24383697e5
Sakib Hasan <shasan@lawdepot.net>
2024-12-05 10:04:41 -0700

    day5

--- Day5/part1.py
+++ Day5/part1.py
@@ -1,3 +1,17 @@
-def part1(input):
-    count = 0
-    return count
+def part1(rules, printed):
+    total_sum = 0
+
+    for l in printed:
+        flag = True
+        for i in range(len(l) - 1):
+            for j in range(i + 1, len(l)):
+                if (l[j], l[i]) in rules:
+                    flag = False
+                    break
+            if not flag:
+                break
+        
+        if flag:
+            total_sum += l[len(l) // 2]
+
+    return total_sum
